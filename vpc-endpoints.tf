8789aac9b5d87d30a5b638141c43c3b0dcf45ce0
Olivier Hoareau <oha@greenberets.io>
2025-03-28 18:41:40 +0100

    fix: up terraform aws provider constraints

--- vpc-endpoints.tf
+++ vpc-endpoints.tf
@@ -1,7 +1,7 @@
 module "vpc-endpoint-ecr" {
   count              = (true == var.ecr_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "ecr"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -11,7 +11,7 @@ module "vpc-endpoint-ecr" {
 module "vpc-endpoint-ecr_dkr" {
   count              = (true == var.ecr_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "ecr_dkr"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -21,7 +21,7 @@ module "vpc-endpoint-ecr_dkr" {
 module "vpc-endpoint-cloudwatch" {
   count              = (true == var.cloudwatch_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "cloudwatch"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -31,7 +31,7 @@ module "vpc-endpoint-cloudwatch" {
 module "vpc-endpoint-s3" {
   count          = (true == var.s3_support) ? 1 : 0
   source         = "genstackio/vpc-endpoint/aws"
-  version        = "0.2.0"
+  version        = "0.3.0"
   service        = "s3"
   vpc_id         = aws_vpc.vpc.id
   route_table_id = aws_route_table.private.id
@@ -40,7 +40,7 @@ module "vpc-endpoint-s3" {
 module "vpc-endpoint-dynamodb" {
   count          = (true == var.dynamodb_support) ? 1 : 0
   source         = "genstackio/vpc-endpoint/aws"
-  version        = "0.2.0"
+  version        = "0.3.0"
   service        = "dynamodb"
   vpc_id         = aws_vpc.vpc.id
   route_table_id = aws_route_table.private.id
@@ -49,7 +49,7 @@ module "vpc-endpoint-dynamodb" {
 module "vpc-endpoint-sns" {
   count              = (true == var.sns_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "sns"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -59,7 +59,7 @@ module "vpc-endpoint-sns" {
 module "vpc-endpoint-sqs" {
   count              = (true == var.sqs_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "sqs"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -69,7 +69,7 @@ module "vpc-endpoint-sqs" {
 module "vpc-endpoint-lambda" {
   count              = (true == var.lambda_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "lambda"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -79,7 +79,7 @@ module "vpc-endpoint-lambda" {
 module "vpc-endpoint-eventbridge" {
   count              = (true == var.eventbridge_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "eventbridge"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -89,7 +89,7 @@ module "vpc-endpoint-eventbridge" {
 module "vpc-endpoint-secretsmanager" {
   count              = (true == var.secretsmanager_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "secretsmanager"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
@@ -99,7 +99,7 @@ module "vpc-endpoint-secretsmanager" {
 module "vpc-endpoint-ses" {
   count              = (true == var.ses_support) ? 1 : 0
   source             = "genstackio/vpc-endpoint/aws"
-  version            = "0.2.0"
+  version            = "0.3.0"
   service            = "ses"
   vpc_id             = aws_vpc.vpc.id
   security_group_ids = local.security_groups
